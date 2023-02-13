b0a3878119b847a224ef2292bd253d9ae506bade
Nirmal1982 <nirmal@digiteka.com>
2023-02-13 09:53:45 +0100

    added webview

--- app/src/main/java/com/example/digitekademoappapplirossel/MainActivity.kt
+++ app/src/main/java/com/example/digitekademoappapplirossel/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.digitekademoappapplirossel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var webview: WebView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        webview = findViewById(R.id.idDtkWebView)
+        webview.apply {
+            settings.javaScriptEnabled = true
+            isScrollContainer=true
+            loadUrl("https://mockup.digiteka.com/mockup_rossel_android.html")
+        }
     }
 }
