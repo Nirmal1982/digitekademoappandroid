098ffe42ae2c509c45036e80298ccf76936773a8
mpinto <miguelpinto@hotmail.co.uk>
2023-08-08 14:24:38 +0200

    Minir fix

--- app/src/main/java/com/example/digitekademoappapplirossel/compose/ArticleWebView.kt
+++ app/src/main/java/com/example/digitekademoappapplirossel/compose/ArticleWebView.kt
@@ -38,8 +38,7 @@ fun ArticleWebView(
                 state.value?.let {
                     val position = layoutCoordinates.positionInWindow().y.absoluteValue.toInt()
 
-                    it.evaluateJavascript(
-                        "console.log(" + position + ")") { }
+                    it.evaluateJavascript("console.log(" + position + ")") { }
                 }
             }
         },
