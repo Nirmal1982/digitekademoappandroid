5213c205636e83d398bec32aa9fe14ada5eb6f5c
mpinto <miguelpinto@hotmail.co.uk>
2023-08-21 16:15:07 +0200

    Added comments to script.

--- app/src/main/assets/webview_position.js
+++ app/src/main/assets/webview_position.js
@@ -1,5 +1,5 @@
 function display_webview_position(webViewPosition, headerHeight, webViewHeight) {
     console.log("Absolute position of webview: " + webViewPosition);
-    console.log("Height of header: " + headerHeight); // header height is the same as footer height
-    console.log("Height of webview: " + webViewHeight);
+    console.log("Height of header: " + headerHeight); // Header height is the same as footer height
+    console.log("Height of webview: " + webViewHeight); // En effet, c'est l'hauteur de la NestedScrollView qui contient la WebView. L'hauteur de la WebView est sa hauteur complète du début de l'article en haut jusqu'à la fin tout en bas.
 }
